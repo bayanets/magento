a0db6e1107fbaf202d353e3bfeb50364673d713c
Yan Malinovsky <yan.malinovsky@gmail.com>
2013-09-20 17:49:50 +0300

    First commit.

---
+++ app/code/local/Yan/HelloWorld/Block/HelloWorld.php
@@ -0,0 +1,6 @@
+<?php
+class Yan_HelloWorld_Block_HelloWorld extends Mage_Core_Block_Template
+{
+    protected $message = "Hello World!";
+  // necessary methods
+}
---
+++ app/code/local/Yan/HelloWorld/controllers/IndexController.php
@@ -0,0 +1,9 @@
+<?php
+class Yan_HelloWorld_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+     $this->loadLayout(array('default'));
+     $this->renderLayout();
+    }
+}
